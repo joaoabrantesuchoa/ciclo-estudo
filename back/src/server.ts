33fffa6ddcccb05cb3b3a72bf717407a4d4846bd
joao-abrantes-uchoa <joao.abrantesuchoa@gmail.com>
2025-05-18 18:34:36 -0300

    fix: solver some erros

--- back/src/server.ts
+++ back/src/server.ts
@@ -5,7 +5,7 @@ import subjectRoutes from './routes/subjectRoutes';
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: process.env.CORS_ORIGIN?.split(",") || "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
